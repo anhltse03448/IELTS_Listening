ce431e9a090c5124646326bfd0217ce666d531d2
Anh Tuan <anhtuan@Anhs-MBP.cigcpe>
2016-10-11 15:45:18 +0700

    init project

---
+++ Hackathon_Tks/Hackathon_Tks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon_Tks
+//
+//  Created by Anh Tuan on 10/11/16.
+//  Copyright © 2016 Anh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hackathon_Tks/Pods/Target Support Files/Pods-Hackathon_Tks/Pods-Hackathon_Tks-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Hackathon_TksVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Hackathon_TksVersionString[];
+
